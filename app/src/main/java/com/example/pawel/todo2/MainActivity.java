cbf40731cb327f06b4c3289d6b9f1536a7708454
Pawel Pisarek <pisarekp73@gmail.com>
2016-12-23 21:27:00 +0100

    async task

--- app/src/main/java/com/example/pawel/todo2/MainActivity.java
+++ app/src/main/java/com/example/pawel/todo2/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,16 @@ import com.example.pawel.todo2.R;
 import com.example.pawel.todo2.db.TaskContract;
 import com.example.pawel.todo2.db.TaskDbHelper;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView mTaskListView;
     private ArrayAdapter<String> mAdapter;
+    private  ArrayList<String> taskList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,96 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            InputStream inputStream = null;
+
+            HttpURLConnection urlConnection = null;
+
+            Integer result = 0;
+            try {
+                /* forming th java.net.URL object */
+                URL url = new URL(params[0]);
+
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                 /* optional request header */
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                /* optional request header */
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+                /* for Get request */
+                urlConnection.setRequestMethod("GET");
+
+                int statusCode = urlConnection.getResponseCode();
+
+                /* 200 represents HTTP OK */
+                if (statusCode == 200) {
+
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+                    String response = convertInputStreamToString(inputStream);
+
+                    Log.d("url z api ", response);
+
+
+//                    ArrayList<String> taskList = new ArrayList<>();
+                    String s = response;
+                    JSONArray recipesArray = new JSONArray(s);
+
+                    Log.d("url",recipesArray.toString());
+                    for (int i = 0; i < recipesArray.length(); ++i) {
+                        JSONObject recipeObject = recipesArray.getJSONObject(i);
+
+                        String recipe = new String(recipeObject.getString("title"));
+
+                        taskList.add(recipe);
+                    }
+
+
+
+                    Log.d("url2",taskList.toString());
+
+
+
+
+                    result = 1; // Successful
+
+                } else {
+                    result = 0; //"Failed to fetch data!";
+                }
+
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return result; //"Failed to fetch data!";
+        }
+    }
+
+    private String convertInputStreamToString(InputStream inputStream) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+
+        String line = "";
+        String result = "";
+
+        while((line = bufferedReader.readLine()) != null){
+            result += line;
+        }
+
+            /* Close Stream */
+        if(null!=inputStream){
+            inputStream.close();
+        }
+
+        return result;
     }
 
     @Override
@@ -81,15 +183,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateUI() {
-        ArrayList<String> taskList = new ArrayList<>();
-        SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-                null, null, null, null, null);
-        while (cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-            Log.d(TAG, "Task: " + cursor.getString(idx));
-            taskList.add(cursor.getString(idx));
+
+        final String url = "http://10.0.2.2:3000/task";
+        new AsyncHttpTask().execute(url);
+
+//        SQLiteDatabase db = mHelper.getReadableDatabase();
+//        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+//                null, null, null, null, null);
+        Log.d("co tu", String.valueOf(taskList));
+        if (taskList.isEmpty()){
+//            ArrayList<String> taskList = new ArrayList<>();
+//            while (cursor.moveToNext()) {
+//                int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+//                Log.d(TAG, "Task: " + cursor.getString(idx));
+//                taskList.add(cursor.getString(idx));
+//            }
+        } else {
+
         }
 
         if (mAdapter == null) {
@@ -104,8 +214,8 @@ public class MainActivity extends AppCompatActivity {
             mAdapter.notifyDataSetChanged();
         }
 
-        cursor.close();
-        db.close();
+//        cursor.close();
+//        db.close();
     }
 
     public void deleteTask(View view) {
