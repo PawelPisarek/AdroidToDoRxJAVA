7c0ed82fc3ecb7cd20b068df50ac76c838a09a2d
Paweł Pisarek <pisarekp73@gmail.com>
2017-01-16 21:20:57 +0100

    POSMAN

--- server/php/hello/index.php
+++ server/php/hello/index.php
@@ -1,6 +1,15 @@
 <?php
 
 /* Send a string after a random number of seconds (2-10) */
-sleep(15);
-echo("sleep two sec");
-?>
+
+$email = $_GET['email'];
+$data='';
+//echo $email;
+header('Content-Type: application/json');
+do {
+    $data2 = file_get_contents('http://localhost:2000/task-new?email='.$email);
+    $data = json_decode($data2, true)['email'];
+//    echo $data;
+
+} while($data=="brak nowych");
+echo $data2;
