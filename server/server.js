d9c8ca57b3b0a775f29957144c202aced5b1fbbf
Paweł Pisarek <pisarekp73@gmail.com>
2017-01-17 16:42:30 +0100

    refactor

--- server/server.js
+++ server/server.js
@@ -14,53 +14,56 @@ var emitter = RxNode.toEventEmitter(source, 'data');
 
 
 app.get('/task', function (req, res) {
-    request2('http://localhost:3000/task', function (error, response, body) {
+    request2('http://localhost:3000/task', function (error, response, body) { //http://localhost:8080/api/messages
         if (!error && response.statusCode == 200) {
             res.send(JSON.parse(body));
         }
     })
 });
+var nowe = {};
 app.post('/task', jsonParser, function (req, res) {
-    // Ensure to call publish to fire events from the observable
+
+    nowe[req.body.sender] = "tak";
+    nowe[req.body.receiver] = "tak";
+
     emitter.publish();
+    var bodyMessage = {};
+    bodyMessage["sender"] = req.body.sender;
+    bodyMessage["content"] = req.body.content;
+    bodyMessage["receiver"] = req.body.receiver;
     request2.post(
         {
-            url: 'http://localhost:3000/task',
+            url: 'http://localhost:3000/task',  //http://localhost:8080/api/messages
             headers: {'Content-Type': 'application/json'},
-            body: '{"title":"' + req.body.title + '"}'
+            body: JSON.stringify(bodyMessage)
         },
         function (error, response, body) {
-            res.status(201).send(JSON.parse('{"title":"' + req.body.title + '","id":"2"}'));
+            res.status(201).send(JSON.parse('{"title":"' + req.body.content + '","id":"dowolne"}'));
 
         }
     );
 
 });
 
-var nowe = [];
+
 setTimeout(function () {
     emitter.on('data', function (data) {
-        nowe = ['1@wp.pl', '2@wp.pl']
+        console.log(nowe);
     });
 }, 0);
 
 
 app.get('/task-new', function (req, res) {
     return new Promise(function (resolve, reject) {
-        var index = nowe.indexOf(req.query.email);
-        if (index > -1) {
-            nowe.splice(index, 1);
+        if (nowe[req.query.email] == "tak") {
+            nowe[req.query.email] = "nie";
             resolve();
         }
-
         emitter.on('end', function () {
             resolve();
         });
     }).then(function () {
-        var index = nowe.indexOf(req.query.email);
-        if (index > -1) {
-            nowe.splice(index, 1);
-        }
+        nowe[req.query.email] = "nie";
         res.send(JSON.parse('{"email":"' + req.query.email + '"}'));
 
     });
